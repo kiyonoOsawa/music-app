b8f4f2b72b60057df7a312be5461695f926e35d1
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2024-05-20 13:02:20 +0900

    add:基本のviewの作成とDiaryCollectionViewの作成(途中)

---
+++ Music/Home/HomeViewController.swift
@@ -0,0 +1,51 @@
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var addDiaryButton: UIButton!
+    @IBOutlet weak var diaryCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        diaryCollectionView.delegate = self
+        diaryCollectionView.dataSource = self
+        setUI()
+    }
+    
+    func setUI() {
+        diaryCollectionView.register(UINib(nibName: "DiaryCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "DiaryCell")
+    }
+}
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCell", for: indexPath) as! DiaryCollectionViewCell
+        cell.layer.cornerRadius = 25
+        cell.layer.shadowColor = UIColor.black.cgColor
+        cell.layer.shadowOpacity = 0.25
+        cell.layer.shadowOffset = CGSize(width: 0, height: 0)
+        cell.layer.shadowRadius = 5
+        cell.layer.masksToBounds = false
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let space: CGFloat = 10
+//        let cellWidth: CGFloat = self.view.frame.width - 76
+//        let cellHeight: CGFloat = diaryCollectionView.frame.height - 60
+        let cellWidth: CGFloat = 344
+        let cellHeight: CGFloat = 128
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 30
+    }
+    
+    
+}
--- Music/TabBarViewController.swift
+++ Music/TabBarViewController.swift
@@ -1,10 +1,3 @@
-//
-//  TabBarViewController.swift
-//  Leaders
-//
-//  Created by 大澤清乃 on 2024/05/19.
-//
-
 import UIKit
 
 class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
@@ -12,7 +5,7 @@ class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         delegate = self
-        UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont.init(name: "HelveticaNeue-Bold", size: 13), .foregroundColor: UIColor(named: "")], for: .selected)
+        UITabBarItem.appearance().setTitleTextAttributes([.font: UIFont.init(name: "HelveticaNeue-Bold", size: 13), .foregroundColor: UIColor(named: "MainColor")], for: .selected)
         tabBar.layer.shadowOffset = CGSize(width: 0, height: 1)
         tabBar.layer.shadowRadius = 6
         tabBar.layer.shadowColor = UIColor.gray.cgColor
